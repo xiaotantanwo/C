365b3684dfa1d134fb5f8d8d926134268967044e
wen <201930390197@mail.scut.edu.cn>
2022-04-15 17:38:32 +0800

    git commit

--- 经典示例/001.第一个C程序/1.c
+++ 经典示例/001.第一个C程序/1.c
@@ -1,6 +1,7 @@
 /* The first C programme */
-#include <stdio.h>				/* ░№║м▒ъ╫╝╩ф╚ы╩ф│Ў═╖╬─╝■ */
-main()						/* ╓ў║п╩¤ */
+#include <stdio.h>				/* я┐╜я┐╜я┐╜я┐╜я┐╜я┐╜╫╝я┐╜я┐╜я┐╜я┐╜я┐╜я┐╜я┐╜═╖я┐╜─╝я┐╜ */
+int main()						/* я┐╜я┐╜я┐╜я┐╜я┐╜я┐╜ */
 {
-		printf("Hello World!\n");	/* ┤Є╙б╩ф│Ў╨┼╧в */
+		printf("Hello World!\n");	/* я┐╜я┐╜╙бя┐╜я┐╜я┐╜я┐╜я┐╜╧в */
+		return 0;
 }
